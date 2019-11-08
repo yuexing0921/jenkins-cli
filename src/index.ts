be18fdf16d34e03b53b1f9c80df5c783e1316cd6
yuexing0921 <yuexing0921@gmail.com>
2019-11-08 17:30:30 +0800

    get jobs

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 
 import * as Jenkins from "jenkins"
 import { JenkinsClientOptions } from "jenkins"
+import { printError } from "./utils"
 
 export interface CliOption {
     lastJob: string;
     config: JenkinsClientOptions;
 }
 
-export const run = (options: CliOption) => {
+export const run = async (options: CliOption) => {
+   
     const jenkins = Jenkins({
         ...options.config,
         promisify: true
     })
+    
+    try{
+        const info = await jenkins.info()
+        const jobs = info.jobs.map(job => job.name)
+    }catch(err){
+        printError(err)
+    }
 
-    jenkins
-        .info()
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,32 @@
+/** @format */
+
+import chalk from 'chalk';
+
+import * as path from 'path';
+
+import {
+  existsSync,
+  readFileSync,
+} from 'fs-extra';
+import { safeLoad } from 'js-yaml';
+
+export const resolve = (p)=> {
+  return path.resolve(p)
+}
+
+
+export const printError = (msg: string) => {
+  console.error(msg)
+  console.log();
+  console.log(chalk.bgRed(msg));
+  console.log();
+};
+
+export const loadYaml = (path: string) => {
+  if (!existsSync(path)) {
+    throw new Error('no such file or directory');
+  }
+
+  const yamlFile = readFileSync(path, 'utf8');
+  return safeLoad(yamlFile);
+};
