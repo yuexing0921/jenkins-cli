f0b3eab4d4717f80a57b734a00f2bc5e7da13ffa
yuexing0921 <yuexing0921@gmail.com>
2019-11-09 17:56:05 +0800

    add loadxml getBranchByRemote

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,11 +4,17 @@ import chalk from 'chalk';
 
 import * as path from 'path';
 
+import { parseString } from "xml2js"
+
+import * as Git from "simple-git/promise";
+
 import {
   existsSync,
   readFileSync,
 } from 'fs-extra';
 import { safeLoad } from 'js-yaml';
+
+const git = Git();
 
 export const resolve = (p)=> {
   return path.resolve(p)
@@ -35,3 +41,23 @@ export const loadYaml = (path: string) => {
   const yamlFile = readFileSync(path, 'utf8');
   return safeLoad(yamlFile);
 };
+
+
+export const loadXML = (xml: string) => {
+  return new Promise((resolve, reject) => {
+    parseString(xml, (err, result) => {
+      if(err) {
+        return reject(err)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export const getBranchByRemote = async (url):Promise<string[]> =>{
+
+  const branch = await git.listRemote(["--heads", url])
+  
+  return branch.match(/refs\/heads\/(\S+)/g) || []
+}
